b196404b2c3a24a1cd2eb368c67d20b4f73b3732
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-04-22 15:41:52 -0400

    Add connection info to index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const { rootCertificates } = require("node:tls");
+
+const connection = mysql.createConnection ({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password1234",
+    database: "employees",
+});
