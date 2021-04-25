486694149d478e6360897328054e0831014547b8
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-04-25 16:32:18 -0400

    Added the rest of the question prompts

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const connection = mysql.createConnection ({
     database: "employeeDB",
 });
 
-const addViewUpdatePrompt = [
+const addViewUpdateDeletePrompt = [
     {
         type: "list",
-        name: "addViewUpdate",
+        name: "addViewUpdateDelete",
         message: "Would you like to view, update, add to, or delete from the employee database?",
         choices: [
             "View",
@@ -24,10 +24,10 @@ const addViewUpdatePrompt = [
     }
 ]
 
-const addViewUpdateAnswer = () => {
-    inquirer.prompt(addViewUpdatePrompt)
+const addViewUpdateDeleteAnswer = () => {
+    inquirer.prompt(addViewUpdateDeletePrompt)
     .then((answer) => {
-        switch (answer.addViewUpdate) {
+        switch (answer.addViewUpdateDelete) {
             case "View":
                 viewQuestions();
                 break;
@@ -48,13 +48,13 @@ const viewQuestionsPrompt = [
     {
         type: "list",
         name: "viewQuestionsAnswer",
-        message: "Would you like to view the department, employee roles, a list of employees, employees by manager, or a department budget?",
+        message: "Would you like to view the departments, employee roles, a list of employees, employees by manager, or a department budget?",
         choices: [
-            "Department",
-            "Employee Roles",
-            "Employee List",
-            "Employees by Manager",
-            "Department Budget"
+            "View Departments",
+            "View Employee Roles",
+            "View Employee List",
+            "View Employees by Manager",
+            "View Department Budget"
         ]
     }
 ]
@@ -63,20 +63,106 @@ function viewQuestions = () => {
     inquirer.prompt(viewQuestionsPrompt)
     .then((answer) => {
         switch (answer.viewQuestionsAnswer) {
-            case "Department":
+            case "View Departments":
                 viewDepartment();
                 break;
-            case "Employee Roles":
+            case "View Employee Roles":
                 viewByEmployeeRoles();
                 break;
-            case "Employee List":
+            case "View Employee List":
                 viewEmployeeList();
                 break;
-            case "Employees by Manager":
+            case "View Employees by Manager":
                 viewEmployeesByManager();
                 break;
-            case "Department Budget":
+            case "View Department Budget":
                 viewDepartmentBudget();
+                break;
+        }
+    })
+}
+
+const updateQuestionsPrompt = [
+    {
+        type: "list",
+        name: "updateQuestionsAnswer",
+        message: "Would you like to view the departments, employee roles, a list of employees, employees by manager, or a department budget?",
+        choices: [
+            "Update Employee Roles",
+            "Update Employee Managers"
+        ]
+    }
+]
+
+function updateQuestions = () => {
+    inquirer.prompt(updateQuestionsPrompt)
+    .then((answer) => {
+        switch (answer.updateQuestionsAnswer) {
+            case "Update Employee Roles":
+                updateEmployeeRoles();
+                break;
+            case "Update Employee Managers":
+                updateEmployeeManagers();
+                break;
+        }
+    })
+}
+
+const addQuestionsPrompt = [
+    {
+        type: "list",
+        name: "addQuestionsAnswer",
+        message: "Would you like to view the departments, employee roles, a list of employees, employees by manager, or a department budget?",
+        choices: [
+            "Add Departments",
+            "Add Employee Roles",
+            "Add Employees",
+        ]
+    }
+]
+
+function addQuestions = () => {
+    inquirer.prompt(addQuestionsPrompt)
+    .then((answer) => {
+        switch (answer.addQuestionsAnswer) {
+            case "Add Departments":
+                addDepartment();
+                break;
+            case "Add Employee Roles":
+                addEmployeeRoles();
+                break;
+            case "Add Employees":
+                addEmployees();
+                break;
+        }
+    })
+}
+
+const deleteQuestionsPrompt = [
+    {
+        type: "list",
+        name: "deleteQuestionsAnswer",
+        message: "Would you like to view the departments, employee roles, a list of employees, employees by manager, or a department budget?",
+        choices: [
+            "Delete Departments",
+            "Delete Employee Roles",
+            "Delete Employees",
+        ]
+    }
+]
+
+function deleteQuestions = () => {
+    inquirer.prompt(deleteQuestionsPrompt)
+    .then((answer) => {
+        switch (answer.deleteQuestionsAnswer) {
+            case "Delete Departments":
+                deleteDepartment();
+                break;
+            case "Delete Employee Roles":
+                deleteEmployeeRoles();
+                break;
+            case "Delete Employees":
+                deleteEmployees();
                 break;
         }
     })
