4131747318b09d4c9ef99d9ff9a8a983fa0421fc
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-04-25 21:59:33 -0400

    Complete schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,29 @@ CREATE database employeeDB;
 USE employeeDB;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL,
     dept_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
-CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    PRIMARY KEY (id)
-)
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
 
-CREATE TABLE employees (
-
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+--    FOREIGN KEY (manager_id) REFERENCES 
 )
 
 
