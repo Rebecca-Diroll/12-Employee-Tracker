d26e535921b856bd6e3c3c9368931c00d3774c4a
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-04-26 20:14:11 -0400

    Completed roleTable

--- db/seed.sql
+++ db/seed.sql
@@ -12,18 +12,18 @@ VALUES
 
 INSERT INTO roleTable(id, title, salary, department_id)
 VALUES
-("Accountant"),
-("Accounting Manager"),
-("Junior Software Engineer"),
-("Senior Software Engineer"),
-("Engineering Manager"),
-("Financial Analyst"),
-("Finance Manager"),
-("Human Resources Coordinator"),
-("Human Resources Manager"),
-("Paralegal"),
-("Lawyer"),
-("General Laborer"),
-("Plant Manager"),
-("Sales Assistant"),
-("Sales Manager")
+(011, "Accountant", 73000, 01),
+(012, "Accounting Manager", 101000, 01),
+(021, "Junior Software Engineer", 65000, 02),
+(022, "Senior Software Engineer", 110000, 02),
+(023, "Engineering Manager", 140000, 02),
+(031, "Financial Analyst", 83000, 03),
+(032, "Finance Manager", 134000, 03),
+(041, "Human Resources Specialist", 63000, 04),
+(042, "Human Resources Manager", 121000, 04),
+(051, "Paralegal", 53000, 05),
+(052, "Lawyer", 127000, 05),
+(061, "General Laborer", 31000, 06),
+(062, "Operations Manager", 103650, 06),
+(071, "Sales Representatives", 86000, 07),
+(072, "Sales Manager", 132000, 07);
