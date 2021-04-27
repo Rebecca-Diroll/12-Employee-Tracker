c2fa776c0fc02068c5c0db3c544bd161213288ec
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-04-26 21:35:25 -0400

    Add data to the employeeTable

--- db/seed.sql
+++ db/seed.sql
@@ -27,3 +27,31 @@ VALUES
 (062, "Operations Manager", 103650, 06),
 (071, "Sales Representatives", 86000, 07),
 (072, "Sales Manager", 132000, 07);
+
+INSERT INTO employeeTable (id, first_name, last_name, role_id, manager_id)
+VALUES
+(2589, "Liam", "Smith"),
+(6324, "Olivia", "Jones"),
+(9623, "Noah", "Johnson"),
+(8961, "Ryan", "Ruiz"),
+(7136, "Samuel", "Gonzalez"),
+(5835, "Emma", "Franklin"),
+(3001, "Oliver", "Williams"),
+(8132, "Ava", "Perez"),
+(6619, "William", "Jones"),
+(4552, "Sophia", "Garcia"),
+(3618, "Elijah", "Miller"),
+(3188, "Isabella", "Davis"),
+(1523, "James", "Wilson"),
+(1580, "Charlotte", "Anderson"),
+(6176, "Benjamin", "Thomas"),
+(6238, "Tyler", "Lopez"),
+(8089, "Amelia", "Taylor"),
+(2656, "Lucas", "Moore"),
+(0681, "Mia", "Jackson"),
+(7461, "Ethan", "Martin"),
+(6372, "Mason", "Lee"),
+(3704, "Harper", "Thompson"),
+(4187, "Ethan", "Harris"),
+(5225, "Evelyn", "Clark"),
+(8468, "Jacob", "Lewis")
