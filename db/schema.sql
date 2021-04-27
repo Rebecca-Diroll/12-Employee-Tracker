d5e4a291e061a53f26712ffa68b78bc7b3a94542
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-04-27 09:25:03 -0400

    Rename several table columns

--- db/schema.sql
+++ db/schema.sql
@@ -1,35 +1,33 @@
 DROP DATABASE IF EXISTS employeeDB;
 CREATE database employeeDB;
 
-USE employeeDB;
+USE employee_db;
 
-CREATE TABLE departmentTable (
-    id INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE dept_table (
+    dept_id INT AUTO_INCREMENT NOT NULL,
     dept_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (dept_id)
 );
 
-CREATE TABLE roleTable (
-    id INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE role_table (
+    role_id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES departmentTable(id)
+    dept_id INT NOT NULL,
+    PRIMARY KEY (role_id),
+    FOREIGN KEY (dept_id) REFERENCES dept_table(dept_id)
 );
 
-CREATE TABLE employeeTable (
-    id INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE employee_table (
+    employee_id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES roleTable(id),
---    FOREIGN KEY (manager_id) REFERENCES 
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (role_id) REFERENCES role_table (role_id),
+    FOREIGN KEY (manager_id) REFERENCES employee_table (employee_id)
 )
-
-
 
 SELECT * FROM top5000;
 select * from top_albums;
